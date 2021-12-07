1b506f8184f192dfc363267b074f903a7892e33f
Hendrik Crause <hcrause@impactradius.com>
2021-12-07 20:49:35 +0200

    Day 6

---
+++ src/template.py
@@ -0,0 +1,21 @@
+def part1(lines):
+    return 0
+
+
+def part2(lines):
+    return 0
+
+
+def main():
+    with open('resources/dayX', 'r') as file:
+        lines = [ l for l in file ]
+        print('Part 1: ' + str(part1(lines)))
+        print('Part 2: ' + str(part2(lines)))
+
+
+if __name__ == '__main__':
+    main()
+
+"""
+
+"""
